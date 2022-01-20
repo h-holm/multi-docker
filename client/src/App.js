a1d858eaffe7b7a9640727a9d6bf5e458b811ff1
Henrik Holm <henholm@kth.se>
2022-01-20 22:21:02 +0100

    Test AWS connection

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Changed greeting text.
+            Changed greeting text
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
